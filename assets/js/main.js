2fa1685e15dbd0297616f2c65e383b33ad57b9fd
JavierAlonso <javi.alonso.gonzalez@gmail.com>
2022-01-06 12:29:50 +0100

    background-header

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,3 +105,30 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
         clickable: true,
     },
 });
+
+/*=================================== SCROLL SECTIONS ACTIVE LINKS ======================================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================================== CHANGE BACKGROUND HEADER =======================================*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
